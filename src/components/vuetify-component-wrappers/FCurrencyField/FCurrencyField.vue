1925e3773596ac04eb637a6dc787bf8670788eac
kjmj <kmorton15@gmail.com>
2022-02-05 11:31:07 -0500

    fix css glitch where dollar sign animates in

--- src/components/vuetify-component-wrappers/FCurrencyField/FCurrencyField.vue
+++ src/components/vuetify-component-wrappers/FCurrencyField/FCurrencyField.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-currency-field prefix="$" v-bind="$attrs" v-on="$listeners"
+  <v-currency-field
+    prefix="$"
+    v-bind="$attrs"
+    v-on="$listeners"
+    :class="applyConditionalCss ? 'v-currency-field-delay' : ''"
     ><slot
   /></v-currency-field>
 </template>
@@ -9,5 +13,23 @@ import Vue from "vue";
 import Component from "vue-class-component";
 
 @Component({})
-export default class FCurrencyField extends Vue {}
+export default class FCurrencyField extends Vue {
+  applyConditionalCss = false;
+  mounted(): void {
+    // add the conditional css class after a delay
+    setTimeout(() => {
+      this.applyConditionalCss = true;
+    }, 350);
+  }
+}
 </script>
+
+<style scoped>
+::v-deep .v-label {
+  transition: none !important;
+}
+
+.v-currency-field-delay ::v-deep .v-label {
+  transition: 0.3s !important;
+}
+</style>
